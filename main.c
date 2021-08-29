9c1aa6a49cb28a02696b4d17c55193b7f7bcfb48
Roman1073741824 <81643230+Roman1073741824@users.noreply.github.com>
2021-08-29 09:53:02 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,32 +3,20 @@
 
 int main()
 {
-    int n = 0;                          //Create a variable for the prime counter;
-    int counter = 0;                    //Create a variable for the divisions without remainder counter;
+    int a = 1;                          //Create a variable for the beginning of the range;
+    int b = 1000000;                    //Create a variable for the ending of the range;
 
-    printf("Prime Numbers Generator\n");//Display the name of the program;
+    printf("Prime Numbers Generator\n");//Display the title of the program;
 
-    for(int i = 1;; ++i)                //Create the loop that will enumerate a dividends;
+    for(int i = a; i <= b; ++i)         //Create the loop that will enumerate a dividends;
     {
-        counter = 0;                    //Reset the divisions without remainder counter;
-
-        for(int j = 1; j <= i; ++j)     //Create the loop that will enumerate a divisors;
+        for(int j = 2; j*j <= i; ++j)   //Create the loop that will enumerate a divisors;
         {
-
             if(i % j == 0)              //If the dividend is divisible by the divisor without remainder;
-            {
-                counter++;              //Increase the value of the divisions without remainder counter by one;
-            }
+                goto m;                 //Go to the next dividend;
         }
-
-        if(counter == 2)                //If the dividend has only two divisors, then it is prime;
-        {
-            printf("%d\n", i);          //Print the prime number;
-
-            if(++n == 1000000)          //If this number is the last;
-            {
-                return 0;               //Stop the program;
-            }
-        }
+        printf("%d\n", i);              //Print the prime number;
+        m:;                             //Go to the next dividend;
     }
+    return 0;                           //End of the program;
 }
